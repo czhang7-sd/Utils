d4f190ba77a64f5191e3970a42efe9f6259fb472
czhang7-sd <czhang6767@gmail.com>
2020-02-14 10:44:04 +0800

    func used for time analysis

---
+++ time_analysis.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+@author: czhang
+@contact: 
+@Time : 2019/9/17 10:13
+"""
+
+import numpy as np
+import time
+import functools
+
+
+def time_wrapper(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result_value = func(*args, **kwargs)
+        end = time.perf_counter()
+        print('{0:<10}.{1:<2} func cost: {2:<8}s'.format(func.__module__, func.__name__, end - start))
+        return result_value
+    return wrapper
+
+@time_wrapper
+def foo(v):
+    return np.exp(v + 3)
+print(foo(3.))
