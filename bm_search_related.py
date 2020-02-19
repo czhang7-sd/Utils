06c22f05b51a614a765cf25a048b7e2fb68e8b11
czhang7-sd <czhang6767@gmail.com>
2020-02-19 11:45:52 +0800

    update func

--- bm_search_related.py
+++ bm_search_related.py
@@ -10,7 +10,13 @@
 import torch
 import torch.nn.functional as F
 
-
+use_cuda = True
+embedding_size = 128
+hidden_size = 512
+vocab_size = 5000
+SOS_ID = 1
+EOS_ID = 2
+max_length = 20
 
 def beam_search(decoder, source_lengths, encoder_outputs, decoder_input, decoder_hidden, stoi, beam_size=3, max_length=10):
     """
@@ -29,6 +35,27 @@ def beam_search(decoder, source_lengths, encoder_outputs, decoder_input, decoder
     for t in range(max_length):
         decoder_output, decoder_hidden = decoder.forward(decoder_input, decoder_hidden, encoder_outputs, source_lengths)
         output_logprob = F.log_softmax(decoder_output, dim=-1)
+        topk_prob, topk_index = torch.topk(output_logprob, beam_size, -1)
+        topk_prob = topk_prob.view(1, -1)
+        topk_index = topk_index.view(1, -1)
+
+        if t== 0:
+            topk_prob_cat = topk_prob
+            topk_index_cat = topk_index
+            topk_score_cat = topk_prob_cat.mean(dim=0)
+            decoder_hidden = decoder_hidden.repeat(1, beam_size, 1)
+            encoder_outputs = encoder_outputs.repeat(1, beam_size, 1)
+        else:
+            topk_prob_cat = topk_prob_cat.view(-1, 1).repeat(1, beam_size).view(t, -1)
+            topk_index_cat = topk_index_cat.view(-1, 1).repeat(1, beam_size).view(t, -1)
+            topk_prob_cat = torch.cat((topk_prob_cat, topk_prob), 0)
+            topk_index_cat = torch.cat((topk_index_cat, topk_index), 0)
+            topk_score_cat = topk_prob_cat.sum(dim=0)
+
+            
+
+
+
 
 
 
